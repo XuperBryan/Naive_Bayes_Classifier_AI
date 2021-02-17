a25aec59685b6abf65c4512dca6d1efac9459e9e
Bryan Xu <bryanbobo123@gmail.com>
2021-02-17 00:49:32 -0800

    bx - maybe? maybe?

--- nbc.h
+++ nbc.h
@@ -95,7 +95,7 @@ using namespace std;
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, type1.5: .8749
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, type0.5: .8742
 
-// age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, !tubed: .8759
+// age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, !tubed: .8585
 
 
 class NaiveBayes {
