13f571657a8dd212d76c07bb66ae308fed29ba4e
Bryan Xu <bryanbobo123@gmail.com>
2021-02-17 00:57:00 -0800

    bx - maybe? maybe?

--- nbc.h
+++ nbc.h
@@ -99,6 +99,7 @@ using namespace std;
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed1.5: .8749
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed0.5: .8645
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2: .8768
+// age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.1: .8769
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2: .8772
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.5: .8758
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed3: .8729
