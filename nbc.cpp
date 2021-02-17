59b5665851ca3517b4cc1b09fac41f944c67fc50
Bryan Xu <bryanbobo123@gmail.com>
2021-02-17 01:13:39 -0800

    bx - maybe? maybe?

--- nbc.cpp
+++ nbc.cpp
@@ -521,14 +521,14 @@ bool NaiveBayes::willDie(int sex, int type, int tubed, int pneumon, int age, int
     // }
     
     if(inmsupr==1){
-        probDie+=2*log(dinmsupr1/dCount);
-        probLive+=2*log(ainmsupr1/aCount);
+        probDie+=.5*log(dinmsupr1/dCount);
+        probLive+=.5*log(ainmsupr1/aCount);
     } else if(inmsupr==2){
-        probDie+=2*log(dinmsupr2/dCount);
-        probLive+=2*log(ainmsupr2/aCount);
+        probDie+=.5*log(dinmsupr2/dCount);
+        probLive+=.5*log(ainmsupr2/aCount);
     } else {
-        probDie+=2*log(dinmsupr3/dCount);
-        probLive+=2*log(ainmsupr3/aCount);
+        probDie+=.5*log(dinmsupr3/dCount);
+        probLive+=.5*log(ainmsupr3/aCount);
     }
     
     if(hyper==1){
--- nbc.h
+++ nbc.h
@@ -121,7 +121,7 @@ using namespace std;
 
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, !inmsupr: .8771
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.5: .8772
-// age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr2: .8772
+// age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr2: .8770
 
 
 
