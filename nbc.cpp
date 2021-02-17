6d15c500bfa76c0547a2e6bc233af4783faba1c1
Bryan Xu <bryanbobo123@gmail.com>
2021-02-16 22:33:38 -0800

    bx - maybe maybe?

--- nbc.cpp
+++ nbc.cpp
@@ -439,22 +439,22 @@ bool NaiveBayes::willDie(int sex, int type, int tubed, int pneumon, int age, int
     } 
 
     if(type==1){
-        probDie+=1.5*log(dtype1/dCount);
-        probLive+=1.5*log(atype1/aCount);
+        probDie+=log(dtype1/dCount);
+        probLive+=log(atype1/aCount);
     } else if(type==2){
-        probDie+=1.5*log(dtype2/dCount);
-        probLive+=1.5*log(atype2/aCount);
+        probDie+=log(dtype2/dCount);
+        probLive+=log(atype2/aCount);
     } 
 
     if(tubed==1){
-        probDie+=log(dtubed1/dCount);
-        probLive+=log(atubed1/aCount);
+        probDie+=1.5*log(dtubed1/dCount);
+        probLive+=1.5*log(atubed1/aCount);
     } else if(tubed==2){
-        probDie+=log(dtubed2/dCount);
-        probLive+=log(atubed2/aCount);
+        probDie+=1.5*log(dtubed2/dCount);
+        probLive+=1.5*log(atubed2/aCount);
     } else {
-        probDie+=log(dtubed3/dCount);
-        probLive+=log(atubed3/aCount);
+        probDie+=1.5*log(dtubed3/dCount);
+        probLive+=1.5*log(atubed3/aCount);
     }
 
     if(pneumon==1){
