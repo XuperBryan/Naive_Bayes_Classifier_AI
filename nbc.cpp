7d2d09cc40db8f65388ea23c85c5622d06104885
Bryan Xu <bryanbobo123@gmail.com>
2021-02-17 01:20:42 -0800

    bx - maybe? maybe?

--- nbc.cpp
+++ nbc.cpp
@@ -532,14 +532,14 @@ bool NaiveBayes::willDie(int sex, int type, int tubed, int pneumon, int age, int
     }
     
     if(hyper==1){
-        probDie+=.85*log(dhyper1/dCount);
-        probLive+=.85*log(ahyper1/aCount);
+        probDie+=1.25*log(dhyper1/dCount);
+        probLive+=1.25*log(ahyper1/aCount);
     } else if(hyper==2){
-        probDie+=.85*log(dhyper2/dCount);
-        probLive+=.85*log(ahyper2/aCount);
+        probDie+=1.25*log(dhyper2/dCount);
+        probLive+=1.25*log(ahyper2/aCount);
     } else {
-        probDie+=.85*log(dhyper3/dCount);
-        probLive+=.85*log(ahyper3/aCount);
+        probDie+=1.25*log(dhyper3/dCount);
+        probLive+=1.25*log(ahyper3/aCount);
     }
     
     if(other==1){
--- nbc.h
+++ nbc.h
@@ -128,6 +128,8 @@ using namespace std;
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.25, !hyper: .8760
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.25, hyper0.5: .8767
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.25, hyper0.75: .8771
+// age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.25, hyper0.85: .8772
+// age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.25, hyper1.25: .8772
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.25, hyper1.5: .8762
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.25, hyper3: .8744
 
