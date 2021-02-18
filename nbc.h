d87df73c5637aa335b1c82f7c104b235feb52f5b
Bryan Xu <bryanbobo123@gmail.com>
2021-02-17 23:18:28 -0800

    bx - testing testing

--- nbc.h
+++ nbc.h
@@ -211,7 +211,7 @@ using namespace std;
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.25, !other, obese1.5, renal0.5, tobacco1.5, covres3, pneumon1.5, !hyper: .879333
 
 // !icu: .877352
-// !icu: .877352
+// icu0.5: .879358
 
 
 
