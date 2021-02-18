2e38bd082811a3548fb061b346ae9d084a5c7734
Bryan Xu <bryanbobo123@gmail.com>
2021-02-17 22:56:26 -0800

    bx - testing testing

--- nbc.cpp
+++ nbc.cpp
@@ -458,14 +458,14 @@ bool NaiveBayes::willDie(int sex, int type, int tubed, int pneumon, int age, int
     }
 
     if(pneumon==1){
-        probDie+=2*log(dpneumon1/dCount);
-        probLive+=2*log(apneumon1/aCount);
+        probDie+=3*log(dpneumon1/dCount);
+        probLive+=3*log(apneumon1/aCount);
     } else if(pneumon==2){
-        probDie+=2*log(dpneumon2/dCount);
-        probLive+=2*log(apneumon2/aCount);
+        probDie+=3*log(dpneumon2/dCount);
+        probLive+=3*log(apneumon2/aCount);
     } else {
-        probDie+=2*log(dpneumon3/dCount);
-        probLive+=2*log(apneumon3/aCount);
+        probDie+=3*log(dpneumon3/dCount);
+        probLive+=3*log(apneumon3/aCount);
     }
     
     if(age <= ageBound){
--- nbc.h
+++ nbc.h
@@ -186,6 +186,7 @@ using namespace std;
 // !pneumon: .8729
 // pneumon0.5: .878139  
 // pneumon1.5: .879333
+// pneumon2: .878927
 
 
 
