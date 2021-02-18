b6f89cfb691872a80007ecb880049ec3555bcb90
Bryan Xu <bryanbobo123@gmail.com>
2021-02-17 23:03:29 -0800

    bx - testing testing

--- nbc.h
+++ nbc.h
@@ -195,6 +195,7 @@ using namespace std;
 // age3, diabete3, !cardio, !copd, !asthma, contact2, sex0.25, !type, tubed2.2, inmsupr1.25, !other, obese1.5, renal0.5, tobacco1.5, covres3, pneumon1.5: .879333
 
 // !preg .878952
+// preg.5 .879028
 
 
 class NaiveBayes {
